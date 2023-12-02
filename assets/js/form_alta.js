83a143414eae56446be7875a634d6bb416950c2e
Mauro Sebastian Ferraresi <msferraresi@gmail.com>
2023-12-02 02:33:59 -0300

    Se corrigen errores, se arregla el formulario de contacto, se crean nuevos js para las funcionalidades varias de la aplicacion, se agregan imagenes necesarias

--- assets/js/form_alta.js
+++ assets/js/form_alta.js
@@ -1,7 +1,5 @@
-const backendPath2 = 'http://localhost:5001';
-
-function SendProduct(){
-    /*document.getElementById('form-response').textContent = '';
+function SendProduct() {
+  /*document.getElementById('form-response').textContent = '';
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
     let mail = document.getElementById("mail").value;
@@ -10,42 +8,44 @@ function SendProduct(){
     let nom_mascota = document.getElementById("nombreMascota").value;
     let consulta = document.getElementById("consulta").value;
 */
-    //limpiar_errores();
+  //limpiar_errores();
 
-    /* if(validar(nombre, 'nombre-error') && validar(apellido, 'apellido-error') && validar(mail, 'mail-error')
+  /* if(validar(nombre, 'nombre-error') && validar(apellido, 'apellido-error') && validar(mail, 'mail-error')
         && validar(edad, 'edad-error') && validar(tipo, 'tipo-error') && validar(nom_mascota, 'mascota-error')
         && validar(consulta, 'consulta-error') && validarEmail(mail, 'mail-error') 
         && validarEntero(edad, 'edad-error')
     ){ */
 
-        let data = {
-            name: "prueba 5",
-            description: "esto es una prueba 5",
-            quantity: "5",
-            price: "50",
-            unit_id: "5",
-            photo_name: "prueba5.png"
-        };
-        //console.log("Nombre: " + nombre + " Apellido: " + apellido + " Mail: " + mail + " Edad: " + edad + " Tipo: " + tipo + " Nombre Mascota: " + nom_mascota + " Consulta: " + consulta);
+  let data = {
+    name: "prueba 5",
+    description: "esto es una prueba 5",
+    quantity: "5",
+    price: "50",
+    unit_id: "5",
+    photo_name: "prueba5.png",
+  };
+  //console.log("Nombre: " + nombre + " Apellido: " + apellido + " Mail: " + mail + " Edad: " + edad + " Tipo: " + tipo + " Nombre Mascota: " + nom_mascota + " Consulta: " + consulta);
 
-        fetch(backendPath2 + '/product', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(result => {
-            document.getElementById('form-response').textContent = 'Su consulta ha sido enviada';
-            console.log(result);
-            //limpiar_errores();
-            //limpiar_campos();
-        })
-        .catch(error => {
-            console.error('Error al enviar la consulta:', error);
-            document.getElementById('form-response').textContent = 'Hubo un error al enviar la consulta';
-            //limpiar_errores();
-        });
-    }
+  fetch(backendPathGeneric + "/product", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      document.getElementById("form-response").textContent =
+        "Su consulta ha sido enviada";
+      console.log(result);
+      //limpiar_errores();
+      //limpiar_campos();
+    })
+    .catch((error) => {
+      console.error("Error al enviar la consulta:", error);
+      document.getElementById("form-response").textContent =
+        "Hubo un error al enviar la consulta";
+      //limpiar_errores();
+    });
+}
 //}
