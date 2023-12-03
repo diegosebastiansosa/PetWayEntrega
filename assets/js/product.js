4f330a13ba753e9a13e903f227ac0e6459feece1
Mauro Sebastian Ferraresi <msferraresi@gmail.com>
2023-12-03 07:23:46 -0300

    Se creo toda la logica para el manejo de los productos

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,4 +1,67 @@
-function buscarProductos() {
+async function openProductModal(isCreate = true, product_id = 0) {
+    const modalTitle = isCreate ? 'Crear Nuevo Producto' : 'Actualizar Producto';
+    const modalElement = document.getElementById("crearProductoModal");
+
+    const modalTitleElement = modalElement.querySelector('.modal-title');
+    modalTitleElement.textContent = modalTitle;
+    uploadUnitOptions();
+    const productForm = document.getElementById("ProductFormModal");
+    productForm.reset();
+    document.getElementById('previewFoto').src = "/assets/img/not_available.png";
+    if(product_id > 0){
+        document.getElementById('btnCreate').style.display = 'none';
+        document.getElementById('btnUpdate').style.display = 'block';
+        const product = await GetProduct(product_id);
+        if (product) {
+            console.log(product)
+            UploadForm(product);
+
+        } else {
+            return
+        }
+    }else{
+        document.getElementById("product_id").textContent = 0
+        document.getElementById('btnCreate').style.display = 'block';
+        document.getElementById('btnUpdate').style.display = 'none';
+    };
+
+    $('#crearProductoModal').modal('show');
+}
+
+function previewImage(){
+    const fotoInput = document.getElementById("foto");
+    const previewFoto = document.getElementById("previewFoto");
+    if (fotoInput.files && fotoInput.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            previewFoto.src = e.target.result;
+        };
+        reader.readAsDataURL(fotoInput.files[0]);
+    } else {
+        previewFoto.src = "/assets/img/not_available.png";
+    }
+}
+
+function UploadForm(product){
+    document.getElementById("product_id").textContent = product.product.id;
+    document.getElementById("photo_name").textContent = product.product.photo_name;
+
+    document.getElementById("nombre").value = product.product.name;
+    document.getElementById("descripcion").value = product.product.description;
+    document.getElementById("cantidad").value = product.product.quantity;
+    document.getElementById("precio").value = product.product.price;
+    document.getElementById("unidad").value = product.product.unit.id;
+
+    const imgElement = document.getElementById("previewFoto");
+
+    if (product.product.image) {
+        imgElement.src = `data:image/png;base64,${product.product.image}`;
+    } else {
+        imgElement.src = "/assets/img/not_available.png";
+    }
+}
+
+function searchProduct() {
     const searchInput = document.getElementById("searchInput");
     const searchValue = searchInput ? searchInput.value : '';
     ListProducts(searchValue);
@@ -24,7 +87,7 @@ function ListProducts(search = null) {
                         <td align="center">${producto.unit.name}</td>
                         <td align="center">${producto.price.toLocaleString('es-AR', { style: 'currency', currency: 'ARS', minimumFractionDigits: 2 })}</td>
                         <td>
-                            <button class="btn btn-warning" onclick="GetProduct(${producto.id})"><i class="fa fa-edit" aria-hidden="true"></i></button>
+                            <button class="btn btn-warning" onclick="openProductModal(false, ${producto.id})"><i class="fa fa-edit" aria-hidden="true"></i></button>
                             <button class="btn btn-danger" onclick="DeleteProduct(${producto.id})"><i class="fa fa-trash" aria-hidden="true"></i></button>
                         </td>
                     `;
@@ -40,11 +103,31 @@ function ListProducts(search = null) {
         });
 }
 
-function GetProduct(product_id) {
+async function GetProduct(product_id) {
+    var url = backendPathGeneric + `/product/${product_id}`;
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
 
+    try {
+        const response = await fetch(url, requestOptions);
+
+        if (!response.ok) {
+            throw new Error(`Error al obtener el producto: ${response.statusText}`);
+        }
+
+        const productData = await response.json();
+        return productData;
+    } catch (error) {
+        console.error('Error al obtener el producto:', error);
+        return null;
+    }
 }
 
-function cargarOpcionesUnidad() {
+function uploadUnitOptions() {
     fetch(backendPathGeneric + "/unit")
         .then(response => response.json())
         .then(data => {
@@ -60,13 +143,6 @@ function cargarOpcionesUnidad() {
         .catch(error => console.error("Error al cargar opciones de unidad:", error));
 }
 
-function abrirCrearProductoModal() {
-    document.getElementById("crearProductoForm").reset();
-    cargarOpcionesUnidad();
-    $('#crearProductoModal').modal('show');
-
-}
-
 function CreateProduct() {
     const name = document.getElementById("nombre").value;
     const description = document.getElementById("descripcion").value;
@@ -75,44 +151,81 @@ function CreateProduct() {
     const unit_id = document.getElementById("unidad").value;
     const fotoInput = document.getElementById("foto");
 
-    var nuevoNombreFoto = "not_available.png"
+    var nuevoNombreFoto = "not_available.png";
+
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("description", description);
+    formData.append("quantity", quantity);
+    formData.append("price", price);
+    formData.append("unit_id", unit_id);
 
     if (fotoInput.files.length > 0) {
-        const fotoFile = fotoInput.files[0];
-        const timestamp = new Date().getTime();
-        nuevoNombreFoto = `${timestamp}.${fotoFile.name.split('.').pop()}`;
+        formData.append("photo", fotoInput.files[0]);
+        nuevoNombreFoto = fotoInput.files[0].name;
     }
+    formData.append("photo_name", nuevoNombreFoto)
 
-    let data = {
-        name: name,
-        description: description,
-        quantity: quantity,
-        price: price,
-        unit_id: unit_id,
-        photo_name: nuevoNombreFoto,
-      };
-
-      fetch(backendPathGeneric + "/product", {
+    fetch(backendPathGeneric + "/product", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      })
+        body: formData,
+    })
         .then((response) => {
             if (!response.ok) {
                 throw new Error(`Error al crear el producto: ${response.statusText}`);
-            }else {
+            } else {
                 console.log("Producto creado correctamente");
-                buscarProductos();
+                searchProduct();
                 $('#crearProductoModal').modal('hide');
             }
         })
         .catch(error => console.error("Error al crear el producto:", error));
 }
 
-function UpdateProduct(product_id) {
+function UpdateProduct() {
+    const product_id = document.getElementById("product_id").textContent;
+    const photo_name = document.getElementById("photo_name").textContent;
 
+    const name = document.getElementById("nombre").value;
+    const description = document.getElementById("descripcion").value;
+    const quantity = document.getElementById("cantidad").value;
+    const price = document.getElementById("precio").value;
+    const unit_id = document.getElementById("unidad").value;
+    const fotoInput = document.getElementById("foto");
+
+    var url =backendPathGeneric + `/product/${product_id}`;
+
+    var nuevoNombreFoto = "not_available.png";
+
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("description", description);
+    formData.append("quantity", quantity);
+    formData.append("price", price);
+    formData.append("unit_id", unit_id);
+
+    if (fotoInput.files.length > 0 && fotoInput.files[0].name != photo_name) {
+        formData.append("photo", fotoInput.files[0]);
+        nuevoNombreFoto = fotoInput.files[0].name;
+    }else{
+        nuevoNombreFoto = photo_name;
+    }
+    formData.append("photo_name", nuevoNombreFoto)
+
+    fetch(url, {
+        method: "PUT",
+        body: formData,
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Error al crear el producto: ${response.statusText}`);
+            } else {
+                console.log("Producto creado correctamente");
+                searchProduct();
+                $('#crearProductoModal').modal('hide');
+            }
+        })
+        .catch(error => console.error("Error al crear el producto:", error));
 }
 
 function DeleteProduct(product_id) {
@@ -133,7 +246,7 @@ function DeleteProduct(product_id) {
                     throw new Error(`Error al eliminar el producto: ${response.statusText}`);
                 }
                 console.log("Producto eliminado correctamente");
-                buscarProductos();
+                searchProduct();
             })
             .catch(error => {
                 console.error('Error al eliminar el producto:', error);
@@ -144,6 +257,6 @@ function DeleteProduct(product_id) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    buscarProductos();
-    cargarOpcionesUnidad();
+    searchProduct();
+    uploadUnitOptions();
 });
